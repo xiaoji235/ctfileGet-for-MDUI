588eb15bd2ea7e09fcf8d7cbe0fb6a61381e48da
xjtx <73387366+xiaoji235@users.noreply.github.com>
2022-10-24 21:08:07 +0800

    分离js

---
+++ js/copy.js
@@ -0,0 +1,81 @@
+﻿			var REFERRER_BLACKLIST = ['https://www.qvocd.cc/', 'https://funletu.com/'];
+			        if (REFERRER_BLACKLIST.includes(document.referrer)) {
+			            alert("禁止从该域名访问本站，该域名存在收费资源分享或强制性公众号引流，却没给我分钱\n你赚钱了，我没赚到钱，那我凭什么给你免费用？\n如果你是用户，你可以直接在新标签页地址栏输入本站域名https://ctfile.qinlili.bid访问\n如果你是站长想解除黑名单，给我打钱就行，你赚钱了我没赚到，那我在自己的地盘上恶心下你是必然的\n\n即将传送到琴梨梨小站首页...");
+			            document.location.href = "https://qinlili.bid";
+			        }
+			        const funDownload = (content, filename) => {
+			            var eleLink = document.createElement('a');
+			            eleLink.download = filename;
+			            eleLink.style.display = 'none';
+			            eleLink.href = content;
+			            document.body.appendChild(eleLink);
+			            eleLink.click();
+			            document.body.removeChild(eleLink);
+			        };
+			        const getInfo = async () => {
+			            document.getElementById("filename").innerText = "";
+			            document.getElementById("filesize").innerText = "";
+			            document.getElementById("filetime").innerText = "";
+			            document.getElementById("dlURL").innerText = ""
+			            document.getElementById("dlbtn").style.display = "none";
+			            password = document.getElementById("passcode").value ? document.getElementById("passcode").value : "547873715";
+			            let fileInfo = await ctfile.getByLink(document.getElementById("link").value, password);
+			            console.log(fileInfo);
+			            if (fileInfo.success) {
+			                document.getElementById("filename").innerText = "文件名称:" + fileInfo.name;
+			                document.getElementById("filesize").innerText = "文件大小:" + fileInfo.size;
+			                document.getElementById("filetime").innerText = "文件时间:" + fileInfo.time;
+			                document.getElementById("dlURL").innerText = "下载地址:" + fileInfo.link;
+			                document.getElementById("dlbtn").style.display = "block";
+			                const record = {
+			                    link: document.getElementById("link").value,
+			                    password,
+			                    queryTime: new Date().toLocaleString(),
+			                    name: fileInfo.name,
+			                    size: fileInfo.size,
+			                    time: fileInfo.time,
+			                }
+			                const store = localStorage.getItem("store")
+			                let records = JSON.parse(store);
+			
+			                if (store && Array.isArray(records)) {
+			                    records = records.filter(r => r.link !== record.link)
+			                    records.push(record);
+			                    localStorage.setItem("store", JSON.stringify(records));
+			                } else {
+			                    localStorage.setItem("store", JSON.stringify([record]))
+			                }
+			            } else {
+			                document.getElementById("filename").innerText = "出错了，错误原因是:" + fileInfo.errormsg;
+			            }
+			        }
+			        const dlFile = () => {
+			            funDownload(dlURL, jsonText.file_name);
+			        }
+			        const readClipboard = async () => {
+			            const log = console.log.bind(console)
+			            const text = await navigator.clipboard.readText().catch(err => {
+							mdui.snackbar({
+								message: '读取剪贴板失败了喵！你是否拒绝了剪贴板权限？',position: 'right-top',
+							});
+			            });
+			            if (text) {
+			                const urlPattern = /(https:\/\/)?[a-z0-9]+\.ctfile\.com\/[a-z]\/([\da-z]+-[a-z\d]+-[a-z\d]+)/;
+			                const pwPattern = /\d{4}/
+			                const $link = document.querySelector("#link")
+			                const $passcode = document.querySelector("#passcode")
+			
+			                try {
+			                    const link = text.match(urlPattern);
+			                    $link.focus()
+			                    $link.value = link[0];
+			                    const rawPassword = text.split("密码")[1] || text.split("密碼")[1];
+			                    const password = rawPassword.match(pwPattern);
+			                    $passcode.focus()
+			                    $passcode.value = password[0];
+			                    await getInfo()
+			                } catch (e) {
+			                    mdui.alert('期望的文本形式类似于“https://url76.ctfile.com/f/20044976-553638227-3a47df 密码4718”，请尝试重新复制或者手动填入', '无法从剪贴板读取链接和密码');
+			                }
+			            }
+			        }
---
+++ js/history.js
@@ -0,0 +1,38 @@
+﻿			const setBlank = () => {
+			            document.querySelector("#records").innerHTML =
+			                `<h4 class="title nodirectory" style="text-align:center;">暂无记录</h4>`
+			        }
+			        const clearAllConfirm = () => {
+			            localStorage.removeItem("store")
+			            setBlank()
+			        }
+			        const writeClipboard = async (element) => {
+			            const link = element.parentElement.dataset.link;
+			            const password = element.parentElement.dataset.pw;
+			            await navigator.clipboard.writeText(`${link}密码: ${password}`);
+			            element.querySelector('.copy').innerText = '已复制';
+			        }
+			        const store = localStorage.getItem("store")
+			        const records = JSON.parse(store)
+			        const $records = document.querySelector("#records")
+			        if (store && Array.isArray(records) && records.length > 0) {
+			            records.forEach(r => {
+			                $records.insertAdjacentHTML("beforeend",
+			                    `<div class="mdui-table">
+								<div class="history-box" data-link="${r.link}" data-pw="${r.password}">
+			                        <h3 class="title">
+			                            <span>${r.name}</span>
+			                            <span class="file-size">${r.size}</span>
+			                        </h3>
+			                        <div class="title history-font">原始链接: ${r.link}</div>
+			                        <div class="title history-font">密码: ${r.password}</div>
+			                        <div class="title history-font">发布时间: ${r.time}</div>
+			                        <div class="title history-font">查询时间: ${r.queryTime}</div>
+			                        <button onclick="writeClipboard(this)" class="mdui-btn mdui-ripple">
+			                            <span class="copy">复制</span>
+			                        </button>
+			                    </div></div><br>`)
+			            })
+			        } else {
+			            setBlank()
+			        }
